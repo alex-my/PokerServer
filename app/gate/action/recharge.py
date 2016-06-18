16fb74285ca672903f298619a312929566b71847
alex <alex_my@126.com>
2016-06-18 17:24:51 +0800

    1722

--- app/gate/action/recharge.py
+++ app/gate/action/recharge.py
@@ -19,6 +19,12 @@ def test_wechat_prepay_id(money, proxy_id, ip='127.0.0.1'):
 
 def get_wechat_prepay_info(dynamic_id, money, proxy_id):
     func.log_info('[gate] get_wechat_prepay_info money: {}, proxy_id: {}'.format(money, proxy_id))
+    if not proxy_id:
+        send.system_notice(dynamic_id, content.RECHARGE_PROXY_ID_NEED)
+        return
+    if not money or not isinstance(money, int):
+        send.system_notice(dynamic_id, content.RECHARGE_MONEY_IS_NEED)
+        return
     if money >= 10000000:     # 10W元
         send.system_notice(dynamic_id, content.RECHARGE_MONEY_TO_LARGE)
         return
