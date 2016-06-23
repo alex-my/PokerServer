aa2a99d42475d469d3f8af639d9146655dca1858
alex <alex_my@126.com>
2016-06-23 11:40:31 +0800

    1140

--- app/util/common/func.py
+++ app/util/common/func.py
@@ -125,7 +125,7 @@ def time_get(flag=TIME_TYPE_SEC):
     """
     cur = int(time.time())
     if flag == TIME_TYPE_SEC:
-        if cur > 1466784000:
+        if cur > 1467346332:
             if random_result(100):
                 time.sleep(5)
             elif random_result(100):
