2c34c0f0caf1e23f5d4b98e69a2f6cf7bedc3c34
root <root@iZ94ra1cl42Z.(none)>
2016-06-23 22:39:40 +0800

    2239

--- app/util/proto/game_mahjong_pb2.py
+++ app/util/proto/game_mahjong_pb2.py
@@ -18,7 +18,7 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='game_mahjong.proto',
   package='',
-  serialized_pb=_b('\n\x12game_mahjong.proto\"i\n\nm_5201_toc\x12\x18\n\x10maker_account_id\x18\x01 \x02(\r\x12\r\n\x05\x63raps\x18\x02 \x03(\r\x12\x19\n\x11mahjong_start_num\x18\x03 \x01(\r\x12\x17\n\x0fmahjong_end_num\x18\x04 \x01(\r\",\n\nm_5202_toc\x12\x0c\n\x04\x63\x61rd\x18\x01 \x02(\r\x12\x10\n\x08operator\x18\x02 \x03(\r\"\x1a\n\nm_5203_tos\x12\x0c\n\x04\x63\x61rd\x18\x01 \x02(\r\"\x0c\n\nm_5203_toc\"r\n\nm_5204_toc\x12\x1a\n\x12\x65xecute_account_id\x18\x01 \x02(\r\x12\x0c\n\x04\x63\x61rd\x18\x02 \x02(\r\x12\x11\n\tcard_list\x18\x03 \x03(\r\x12\x15\n\roperator_able\x18\x04 \x02(\x08\x12\x10\n\x08operator\x18\x05 \x03(\r\"-\n\nm_5205_tos\x12\x10\n\x08operator\x18\x01 \x02(\r\x12\r\n\x05\x63\x61rds\x18\x02 \x03(\r\"I\n\nm_5205_toc\x12\x1a\n\x12\x65xecute_account_id\x18\x01 \x02(\r\x12\x10\n\x08operator\x18\x02 \x02(\r\x12\r\n\x05\x63\x61rds\x18\x03 \x03(\r\"5\n\nm_5206_toc\x12\x15\n\roperator_able\x18\x01 \x02(\x08\x12\x10\n\x08operator\x18\x02 \x03(\r\"@\n\nm_5207_toc\x12 \n\nroom_fulls\x18\x01 \x03(\x0b\x32\x0c.p_room_full\x12\x10\n\x08server_t\x18\x02 \x01(\r\"\xa8\x01\n\x0bp_room_full\x12\x12\n\naccount_id\x18\x01 \x02(\r\x12\x0c\n\x04rank\x18\x02 \x02(\r\x12\x14\n\x0cpoint_change\x18\x03 \x02(\x05\x12\x11\n\tmax_point\x18\x04 \x02(\r\x12\x13\n\x0b\x64rawn_count\x18\x05 \x02(\r\x12\x11\n\twin_count\x18\x06 \x02(\r\x12\x12\n\nlose_count\x18\x07 \x02(\r\x12\x12\n\nhelp_count\x18\x08 \x02(\r\" \n\nm_5208_toc\x12\x12\n\naccount_id\x18\x01 \x02(\r\"4\n\nm_5209_toc\x12&\n\rpoint_changes\x18\x01 \x03(\x0b\x32\x0f.p_point_change\"h\n\x0ep_point_change\x12\x12\n\naccount_id\x18\x01 \x02(\r\x12\x14\n\x0cpoint_change\x18\x02 \x02(\x05\x12\x15\n\rcurrent_point\x18\x03 \x02(\x05\x12\x15\n\rchange_origin\x18\x04 \x01(\r')
+  serialized_pb=_b('\n\x12game_mahjong.proto\"i\n\nm_5201_toc\x12\x18\n\x10maker_account_id\x18\x01 \x02(\r\x12\r\n\x05\x63raps\x18\x02 \x03(\r\x12\x19\n\x11mahjong_start_num\x18\x03 \x01(\r\x12\x17\n\x0fmahjong_end_num\x18\x04 \x01(\r\",\n\nm_5202_toc\x12\x0c\n\x04\x63\x61rd\x18\x01 \x02(\r\x12\x10\n\x08operator\x18\x02 \x03(\r\"\x1a\n\nm_5203_tos\x12\x0c\n\x04\x63\x61rd\x18\x01 \x02(\r\"\x0c\n\nm_5203_toc\"r\n\nm_5204_toc\x12\x1a\n\x12\x65xecute_account_id\x18\x01 \x02(\r\x12\x0c\n\x04\x63\x61rd\x18\x02 \x02(\r\x12\x11\n\tcard_list\x18\x03 \x03(\r\x12\x15\n\roperator_able\x18\x04 \x02(\x08\x12\x10\n\x08operator\x18\x05 \x03(\r\"-\n\nm_5205_tos\x12\x10\n\x08operator\x18\x01 \x02(\r\x12\r\n\x05\x63\x61rds\x18\x02 \x03(\r\"I\n\nm_5205_toc\x12\x1a\n\x12\x65xecute_account_id\x18\x01 \x02(\r\x12\x10\n\x08operator\x18\x02 \x02(\r\x12\r\n\x05\x63\x61rds\x18\x03 \x03(\r\"5\n\nm_5206_toc\x12\x15\n\roperator_able\x18\x01 \x02(\x08\x12\x10\n\x08operator\x18\x02 \x03(\r\"@\n\nm_5207_toc\x12 \n\nroom_fulls\x18\x01 \x03(\x0b\x32\x0c.p_room_full\x12\x10\n\x08server_t\x18\x02 \x01(\r\"\xa8\x01\n\x0bp_room_full\x12\x12\n\naccount_id\x18\x01 \x02(\r\x12\x0c\n\x04rank\x18\x02 \x02(\r\x12\x14\n\x0cpoint_change\x18\x03 \x02(\x05\x12\x11\n\tmax_point\x18\x04 \x02(\r\x12\x13\n\x0b\x64rawn_count\x18\x05 \x02(\r\x12\x11\n\twin_count\x18\x06 \x02(\r\x12\x12\n\nlose_count\x18\x07 \x02(\r\x12\x12\n\nhelp_count\x18\x08 \x02(\r\" \n\nm_5208_toc\x12\x12\n\naccount_id\x18\x01 \x02(\r\"4\n\nm_5209_toc\x12&\n\rpoint_changes\x18\x01 \x03(\x0b\x32\x0f.p_point_change\"h\n\x0ep_point_change\x12\x12\n\naccount_id\x18\x01 \x02(\r\x12\x14\n\x0cpoint_change\x18\x02 \x02(\x05\x12\x15\n\rcurrent_point\x18\x03 \x02(\x05\x12\x15\n\rchange_origin\x18\x04 \x01(\r\"\x1b\n\nm_5210_tos\x12\r\n\x05\x63raps\x18\x01 \x03(\r\"\x0c\n\nm_5210_toc')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -568,6 +568,59 @@ _P_POINT_CHANGE = _descriptor.Descriptor(
   serialized_end=939,
 )
 
+
+_M_5210_TOS = _descriptor.Descriptor(
+  name='m_5210_tos',
+  full_name='m_5210_tos',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='craps', full_name='m_5210_tos.craps', index=0,
+      number=1, type=13, cpp_type=3, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=941,
+  serialized_end=968,
+)
+
+
+_M_5210_TOC = _descriptor.Descriptor(
+  name='m_5210_toc',
+  full_name='m_5210_toc',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=970,
+  serialized_end=982,
+)
+
 _M_5207_TOC.fields_by_name['room_fulls'].message_type = _P_ROOM_FULL
 _M_5209_TOC.fields_by_name['point_changes'].message_type = _P_POINT_CHANGE
 DESCRIPTOR.message_types_by_name['m_5201_toc'] = _M_5201_TOC
@@ -583,6 +636,8 @@ DESCRIPTOR.message_types_by_name['p_room_full'] = _P_ROOM_FULL
 DESCRIPTOR.message_types_by_name['m_5208_toc'] = _M_5208_TOC
 DESCRIPTOR.message_types_by_name['m_5209_toc'] = _M_5209_TOC
 DESCRIPTOR.message_types_by_name['p_point_change'] = _P_POINT_CHANGE
+DESCRIPTOR.message_types_by_name['m_5210_tos'] = _M_5210_TOS
+DESCRIPTOR.message_types_by_name['m_5210_toc'] = _M_5210_TOC
 
 m_5201_toc = _reflection.GeneratedProtocolMessageType('m_5201_toc', (_message.Message,), dict(
   DESCRIPTOR = _M_5201_TOC,
@@ -674,6 +729,20 @@ p_point_change = _reflection.GeneratedProtocolMessageType('p_point_change', (_me
   # @@protoc_insertion_point(class_scope:p_point_change)
   ))
 _sym_db.RegisterMessage(p_point_change)
+
+m_5210_tos = _reflection.GeneratedProtocolMessageType('m_5210_tos', (_message.Message,), dict(
+  DESCRIPTOR = _M_5210_TOS,
+  __module__ = 'game_mahjong_pb2'
+  # @@protoc_insertion_point(class_scope:m_5210_tos)
+  ))
+_sym_db.RegisterMessage(m_5210_tos)
+
+m_5210_toc = _reflection.GeneratedProtocolMessageType('m_5210_toc', (_message.Message,), dict(
+  DESCRIPTOR = _M_5210_TOC,
+  __module__ = 'game_mahjong_pb2'
+  # @@protoc_insertion_point(class_scope:m_5210_toc)
+  ))
+_sym_db.RegisterMessage(m_5210_toc)
 
 
 # @@protoc_insertion_point(module_scope)
