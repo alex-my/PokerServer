1f5294c43412108b32b2dfc739a18cf63a35226f
alex <alex_my@126.com>
2016-06-25 12:56:13 +0800

    3

--- app/util/defines/operators.py
+++ app/util/defines/operators.py
@@ -7,9 +7,10 @@ USER_OPERATOR_READY = 1             # 准备
 USER_OPERATOR_OFFLINE = 2           # 暂离
 USER_OPERATOR_CLOSE = 3             # 解散房间
 USER_OPERATOR_SWITCH = 4            # 切牌
-
+USER_OPERATOR_WARN = 5              # 报警
 USER_OPERATOR_BACK = 6              # 切到后台
 USER_OPERATOR_FRONT = 7             # 回到前台
+USER_OPERATOR_NONE = 9              # 无(正常)
 
 
 
